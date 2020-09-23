f3d87026bcc331f3acb6c9eb4c4c604ef471e512
Cesar Roman <thecesrom@Cesars-MacBook-Air.local>
2020-09-23 08:52:19 -0700

    Initial commit

---
+++ src/dataset.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2020
+# Author: Cesar Roman
+# Contact: thecesrom@gmail.com
+
+"""Dataset module."""
+
+__all__ = [
+    'to_xml'
+]
+
+import system.dataset
+
+
+def to_xml(dataset, root='root', element='row'):
+    """Returns a string XML representation of the Dataset.
+
+    Args:
+        dataset (Dataset): The input dataset.
+        root (str): The value of the root. If not provided, it defaults to
+            "root".
+        element (str): The value of the row. If not provided, it defaults to
+            "row".
+
+    Returns:
+        str: The string XML representation of the dataset.
+    """
+    headers = system.dataset.getColumnHeaders(dataset)
+    data = system.dataset.toPyDataSet(dataset)
+    new_line = '\n'
+    tab = ' ' * 4
+    ret_str = '<{}>{}'.format(root, new_line)
+
+    for row in data:
+        ret_str += '{}<{}>{}'.format(tab, element, new_line)
+        for header in headers:
+            ret_str += '{}<{}>{}</{}>{}'.format(
+                tab * 2, header, row[header], header, new_line
+            )
+        ret_str += '{}</{}>{}'.format(tab, element, new_line)
+    ret_str += '</{}>'.format(root)
+
+    return ret_str
---
+++ src/exceptions.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2020
+# Author: Cesar Roman
+# Contact: thecesrom@gmail.com
+
+"""Exceptions module."""
+
+__all__ = [
+    'ApplicationError',
+    'TagError'
+]
+
+
+class ApplicationError(Exception):
+    """Application Error class."""
+
+    def __init__(self,
+                 message,
+                 inner_exception=None,
+                 cause=None):
+        """Application Error initializer.
+
+        Args:
+            message (str): The error message.
+            inner_exception (object): The inner Exception. Optional.
+            cause (object): The cause of the Exception. Optional.
+        """
+        super(ApplicationError, self).__init__(message)
+        self.inner_exception = inner_exception
+        self.cause = cause
+
+    def __repr__(self):
+        return "{}({!r}, {!r}, {!r})".format(
+            self.__class__.__name__,
+            repr(self.message),
+            self.inner_exception.__repr__(),
+            repr(self.cause)
+        )
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class TagError(Exception):
+    """Tag Erro class."""
+
+    def __init__(self,
+                 message):
+        """Tag Error initializer.
+
+        Args:
+            message (str): The error message.
+        """
+        super(TagError, self).__init__(message)
+
+    def __repr__(self):
+        return "{}({!r})".format(
+            self.__class__.__name__,
+            repr(self.message)
+        )
+
+    def __str__(self):
+        return repr(self.message)
