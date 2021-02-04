42354d896dab7d9a2b4425551d53a4ad7d6222ad
César Román <thecesrom@gmail.com>
2021-02-04 15:46:44 -0800

    Adding project files

---
+++ ignition/script-python/incendium/__init__/code.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020
+# Author: Cesar Roman
+# Contact: cesar@thecesrom.dev
+#
+# Licensed under the MIT License.
+#
+# https://github.com/thecesrom/incendium/blob/main/LICENSE
+#
+"""incendium.
+
+incendium is a package that extends and wraps some functions from
+Ignition Scripting API.
+
+For more information, please refer to the Wiki.
+https://github.com/thecesrom/incendium/wiki
+"""
+
+__version__ = '1.0.3'
