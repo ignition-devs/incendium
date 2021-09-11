b76db29cc6dc7e365546e5accbb37ef87c13ffb3
César Román <thecesrom@gmail.com>
2021-09-11 12:52:44 -0700

    feat(release): v1.0.7
    add:
    - option to install using `jython -m pip install`

---
+++ src/incendium/__version__.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# Copyright (C) 2018-2021
+# Author: Cesar Roman
+# Contact: cesar@thecesrom.dev
+
+"""Package information."""
+
+__title__ = "incendium"
+__description__ = "Package that extends and wraps Ignition Scripting API."
+__url__ = "https://github.com/thecesrom/incendium"
+__version__ = "1.0.7"
+__author__ = "César Román"
+__author_email__ = "cesar@thecesrom.dev"
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2018-2021 César Román"
