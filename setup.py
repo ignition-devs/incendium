8c23ce9c23b146374e16b7da349c1e3e992044ac
César Román <thecesrom@gmail.com>
2021-09-13 08:51:12 -0700

    fix(setup): include `__cycle__` in package `version`

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ with open("README.md", "r", "utf-8") as f:
 
 setup(
     name=about["__title__"],
-    version=about["__version__"],
-    description="{}{}".format(about["__description__"], about["__cycle__"]),
+    version="{}{}".format(about["__version__"], about["__cycle__"]),
+    description=about["__description__"],
     long_description=readme,
     long_description_content_type="text/markdown",
     url=about["__url__"],
