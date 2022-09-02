f4dbd2cc63b6643ec14207a89977fbfa30f377af
César Román <thecesrom@gmail.com>
2022-09-02 12:31:13 -0700

    feat(date): add functions to get first and last day of the month

--- src/incendium/date.py
+++ src/incendium/date.py
@@ -1,7 +1,8 @@
 """Date module."""
 
-__all__ = ["compare"]
+__all__ = ["compare", "get_first_day_of_month", "get_last_day_of_month"]
 
+import system.date
 from java.util import Date
 
 
@@ -25,3 +26,34 @@ def compare(date_1, date_2):
         ret_val = -1
 
     return ret_val
+
+
+def get_first_day_of_month(date):
+    # type: (Date) -> Date
+    """Get first day of the month at midnight given the date.
+
+    Args:
+        date: The date.
+
+    Returns:
+        The date set to the first date of the month at midnight.
+    """
+    year = system.date.getYear(system.date.midnight(date))
+    month = system.date.getMonth(date)
+    return system.date.getDate(year, month, 1)
+
+
+def get_last_day_of_month(date):
+    # type: (Date) -> Date
+    """Get last day of the month at 11:59:59 PM given the date.
+
+    Args:
+        date: The date.
+
+    Returns:
+        The date set to the last day of the month at 11:59:59 PM.
+    """
+    next_month = system.date.addMonths(system.date.midnight(date), 1)
+    year = system.date.getYear(next_month)
+    month = system.date.getMonth(next_month)
+    return system.date.addSeconds(system.date.getDate(year, month, 1), -1)
